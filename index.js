239f4b76769711e6303ca47e3c81d786548e98bd
jvanosdol <j.vanosdol99@yahoo.com>
2022-08-26 16:12:50 -0700

    more oop practice

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-class User {
-    constructor(email, name) {
-        this.email = email;
-        this.name = name;
-        this.score = 0;
-    }
-    login() {
-        console.log(this.email, 'just logged in')
-        return this;
-    }
-    logout() {
-        console.log(this.email, 'just logged out')
-        return this;
-    }
-    updateScore() {
-        this.score++;
-        console.log(this.email, 'score is now', this.score);
-        return this;
-    }
+function User(email, name, status) {
+    this.email = email;
+    this.name = name;
+    this.online = false;
 }
 
-let userOne = new User('j.vanosdol99@yahoo.com', 'james');
-let userTwo = new User('yoshi@mariokorp', 'Yoshi');
+User.prototype.login = function() {
+    this.online = true;
+    console.log(this.email, 'has logged in');
+}
 
-userOne.login().updateScore().updateScore().logout();
+User.prototype.logout = function() {
+    this.online = false;
+    console.log(this.email, 'has logged out')
+}
+
+function Admin(...args) {
+    User.apply(this, args);
+}
+
+let adminEmail = 'j.vanosdol';
+let adminName = 'james'
+let adminStatus = true;
+
+let userOne = new User('j.vanosdol99@yahoo.com', 'James')
+let admin = new Admin(adminEmail, adminName, adminStatus)
+
+
+
+console.log(admin);
