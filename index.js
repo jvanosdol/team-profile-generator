20724097f2afc9c6cbb146a3d6975ff94afa64d4
jvanosdol <j.vanosdol99@yahoo.com>
2022-08-26 17:05:25 -0700

    added employee and employee role files in lib folder

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
-function User(email, name, status) {
-    this.email = email;
-    this.name = name;
-    this.online = false;
+const book1 = {
+    title: 'harry potter',
+    author: 'JK Rowling',
+    year: '2001',
+    getSummary: function() {
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    }
 }
 
-User.prototype.login = function() {
-    this.online = true;
-    console.log(this.email, 'has logged in');
-}
-
-User.prototype.logout = function() {
-    this.online = false;
-    console.log(this.email, 'has logged out')
-}
-
-function Admin(...args) {
-    User.apply(this, args);
-}
-
-let adminEmail = 'j.vanosdol';
-let adminName = 'james'
-let adminStatus = true;
-
-let userOne = new User('j.vanosdol99@yahoo.com', 'James')
-let admin = new Admin(adminEmail, adminName, adminStatus)
-
-
-
-console.log(admin);
+console.log(Object.keys(book1))
+console.log(book1.getSummary())
