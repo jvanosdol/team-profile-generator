27dd0ed7d2f1ae11d94c9582b04fba6eed0a969c
jvanosdol <j.vanosdol99@yahoo.com>
2022-08-26 18:01:31 -0700

    added inital testing files

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-class Employee {
-    constructor(name, id, email) {
-        this.name = name;
-        this.id = id; 
-    }
-}
-
-class Intern extends Employee {
-
-}
-
-class Manager extends Employee {
-
-}
-
-class Engineer extends Employee {
-
-}
+//
