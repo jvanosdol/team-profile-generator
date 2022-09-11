deef3ed874bc6ed912422ed4191c783cdad85cee
jvanosdol <j.vanosdol99@yahoo.com>
2022-09-11 16:56:21 -0700

    finished intern testing

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -1,56 +1,49 @@
 const Engineer = require("../lib/engineer.js");
 
 
+// needs to test name, id, email, getGithub, and getRole()
+
 describe("Engineer", () => {
 
   
-// checks to make sure this is infact an object
-test('makes sure that the engineer class can be created', () => {
-  const engineerTest = new Engineer();
-  expect(typeof(engineerTest)).toBe('object')
-})
+    // checks to make sure this is infact an object
+    test('makes sure that the engineer class can be created', () => {
+      const engineerTest = new Engineer();
+      expect(typeof(engineerTest)).toBe('object')
+    })
 
-// checks for engineer name
-test('Can enter in an Engineer\'s name.', () => {
-  const testName = 'James';
-  const engineerTest = new Engineer(testName);
-  expect(engineerTest.name).toBe(testName)
-})
+    // checks for engineer name
+    test('Can enter in an Engineer\'s name.', () => {
+      const testName = 'James';
+      const engineerTest = new Engineer(testName);
+      expect(engineerTest.name).toBe(testName)
+    })
 
-// checks for engineer name
-test('Can enter in an Engineer\'s id.', () => {
-  const testId = '1234';
-  const engineerTest = new Engineer('placeholder', testId);
-  expect(engineerTest.id).toBe(testId)
-})
+    // checks for engineer id
+    test('Can enter in an Engineer\'s id.', () => {
+      const testId = '1234';
+      const engineerTest = new Engineer('placeholder', testId);
+      expect(engineerTest.id).toBe(testId)
+    })
 
 
-// checks for engineer email
-test('Can enter in an Engineer\'s email.', () => {
-  const testEmail = 'test@test.com';
-  const engineerTest = new Engineer('placeholder', 'placeholder', testEmail);
-  expect(engineerTest.email).toBe(testEmail)
-})
+    // checks for engineer email
+    test('Can enter in an Engineer\'s email.', () => {
+      const testEmail = 'test@test.com';
+      const engineerTest = new Engineer('placeholder', 'placeholder', testEmail);
+      expect(engineerTest.email).toBe(testEmail)
+    })
 
-// checks for engineer github
-test('Can enter in an Engineer\'s github account.', () => {
-  const testGithub = 'test@github.com';
-  const engineerTest = new Engineer('placeholder', 'placeholder', 'placeholder', testGithub);
-  expect(engineerTest.github).toBe(testGithub)
-})
+    // checks if engineer 'getGithub' method works
+    test('Can retrieve Engineer\'s GitHub account.', () => {
+      const engineerTest = new Engineer('placeholder', 'placeholder', 'placeholder', 'test@github.com');
+      expect(engineerTest.getGithub()).toBe('test@github.com')
+    })
 
-// checks if engineer 'getGithub' method works
-test('Can retrieve Engineer\'s GitHub account.', () => {
-  const engineerTest = new Engineer('placeholder', 'placeholder', 'placeholder', 'test@github.com');
-  expect(engineerTest.getGithub()).toBe('test@github.com')
-})
-
-// checks if engineer 'getRole' method works
-test('Can retrieve Engineer\'s name.', () => {
-  const engineerTest = new Engineer('James');
-  expect(engineerTest.getName()).toBe('James')
-})
-
-// need to test email, github, getGithub, and getRole()
+    // checks if engineer 'getRole' method works
+    test('Can retrieve Engineer\'s role.', () => {
+      const engineerTest = new Engineer('Engineer');
+      expect(engineerTest.getRole()).toBe('Engineer')
+    })
 
 })
--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -1,15 +1,49 @@
-const Engineer = require("../lib/engineer.js");
+const Intern = require("../lib/intern.js");
 
-test('makes sure that the engineer class can be created', () => {
-  const engineerTest = new Engineer();
-  expect(typeof(engineerTest)).toBe('object')
+
+// needs to test name, id, email, getSchool(), and getRole()
+
+describe("Intern", () => {
+
+  
+    // checks to make sure this is infact an object
+    test('makes sure that the intern class can be created', () => {
+      const internTest = new Intern();
+      expect(typeof(internTest)).toBe('object')
+    })
+
+    // checks for intern name
+    test('Can enter in an Intern\'s name.', () => {
+      const testName = 'James';
+      const internTest = new Intern(testName);
+      expect(internTest.name).toBe(testName)
+    })
+
+    // checks for intern id
+    test('Can enter in an Intern\'s id.', () => {
+      const testId = '1234';
+      const internTest = new Intern('placeholder', testId);
+      expect(internTest.id).toBe(testId)
+    })
+
+
+    // checks for intern email
+    test('Can enter in an Intern\'s email.', () => {
+      const testEmail = 'test@test.com';
+      const internTest = new Intern('placeholder', 'placeholder', testEmail);
+      expect(internTest.email).toBe(testEmail)
+    })
+
+    // checks if intern 'getSchool' method works
+    test('Can retrieve Intern\'s school.', () => {
+      const internTest = new Intern('placeholder', 'placeholder', 'placeholder', 'UCLA');
+      expect(internTest.getSchool()).toBe('UCLA')
+    })
+
+    // checks if intern 'getRole' method works
+    test('Can retrieve Intern\'s role.', () => {
+      const internTest = new Intern('Intern');
+      expect(internTest.getRole()).toBe('Intern')
+    })
+
 })
-
-test('Can enter in an Engineer\'s name.', () => {
-  const name = 'James';
-  const engineerTest = new Engineer(name);
-  expect(engineerTest.name).toBe(name)
-})
-
-// need to test email, github, getGithub, and getRole()
-
